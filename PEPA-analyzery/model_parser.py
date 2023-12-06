fcd150168b8c2cac3bb33295bcbf24097f38059b
Matteo Facci <36844147+faccimatteo@users.noreply.github.com>
2023-12-07 00:49:43 +0100

    Added outgoing states computation

--- PEPA-analyzery/model_parser.py
+++ PEPA-analyzery/model_parser.py
@@ -1,6 +1,7 @@
 import re
 import json
 from typing import List
+from itertools import product
 
 def remove_comments(input_string: str):
     # Remove /* */ comments
@@ -41,36 +42,85 @@ def remove_unneeded_characters(input_string: str) -> str:
     input_string = remove_whitespaces(input_string=input_string)
     return input_string
 
-def populate_components(incoming_component: str, components_actions: List[str], components_parsed: dict) -> dict:
+def populate_components(component: str, components_actions: List[str], components_parsed: dict) -> dict:
     
     if len(components_actions) > 0:
             
         next_state_activity = components_actions[0].split('.')[0].split(',')[0][1:] # Taking activity value
         next_state_rate = components_actions[0].split('.')[0].split(',')[1][:-1] # Taking rate value
         next_state = components_actions[0].split('.')[1] # Taking target component 
-        next_step = {'transition': incoming_component + " -> " +  next_state_activity, 'rate': next_state_rate}
+        next_step = {'next_state': next_state, 'rate': next_state_rate}
         
-        if next_state not in components_parsed:
+        if component not in components_parsed:
+            components_parsed[component] = {}
             # If components does not have associated incoming transitions
-            components_parsed[next_state] = [next_step]
+            if next_state_activity not in components_parsed[component]:
+                components_parsed[component][next_state_activity] = [next_step]
+            else: 
+                components_parsed[component][next_state_activity].append(next_step)
+ 
         else:
             # If components already has associated incoming transitions
-            components_parsed[next_state].append(next_step)
-        
+            if next_state_activity not in components_parsed[component]:
+                components_parsed[component][next_state_activity] = [next_step]
+            else: 
+                components_parsed[component][next_state_activity].append(next_step)  
+                           
         # Removing heading action
         components_actions = components_actions[1:]
-        populate_components(incoming_component=incoming_component, components_actions=components_actions, components_parsed=components_parsed)
+        populate_components(component=component, components_actions=components_actions, components_parsed=components_parsed)
     else:
         return components_parsed
  
 def parse_components(component: str, component_value: str, components: dict) -> dict:
+    """
+    Recursively extract PEPA model components with their relatives transitions in a dictionary
+
+    Args:
+        component (str): component's name
+        component_value (str): list of all component activities
+        components (dict): components dictionary to populate by parsing component's activities
+    Returns:
+        _type_: _description_
+    """
+    
     components_actions = component_value.split('+')
     
-    populate_components(incoming_component=component, components_actions=components_actions, components_parsed=components) 
+    populate_components(component=component, components_actions=components_actions, components_parsed=components) 
      
     return components
+
+
+def compute_outgoing_states(current_state: List[str], activity: str, model: dict):
+    """ 
+    Compute every outgoing states starting from a `current_sate` and given an `activity`.
+    Transitions are followed by using `model` as reference.
     
-def parse_model(filePath: str):
+    Args:
+        current_state (List[str]): list of current PEPA components of the system at a given time T.
+        activity (str): 
+        model (dict): _description_
+    """
+    component_next_state = []
+    is_set_present = False
+    for component in current_state:
+        if len(model["components"][component][activity]) > 1:
+            is_set_present = True
+            multiple_states_outcomes = set([outgoing_component_state['next_state'] for outgoing_component_state in model["components"][component][activity]])
+            component_next_state.append(multiple_states_outcomes) 
+        else: 
+            component_next_state.append(model["components"][component][activity][0]["next_state"])
+        
+    if is_set_present: 
+        # Convert to set if not already a set
+        sets = [elem if isinstance(elem, set) else {elem} for elem in component_next_state]
+        # Calculate combinations
+        component_next_state = list(map(lambda x: list(x), list(product(*sets))))
+        
+    print(activity)
+    print(component_next_state)
+            
+def parse_model(filePath: str) -> dict:
     
     try:
         file = open(file=filePath)
@@ -111,5 +161,17 @@ def parse_model(filePath: str):
             # Parsing components 
             parse_components(component=entity, component_value=value, components=model["components"]) 
             
-    print(json.dumps(model, sort_keys=True, indent=4)) 
-parse_model("/Users/matteo/Documents/Unive/FMFSV/PEPA-analyzery/model.pepa")
+    return model 
+    
+    
+
+model = parse_model("/Users/matteo/Documents/Unive/FMFSV/PEPA-analyzery/model.pepa")
+
+current_state = ["MUn", "EnvUn", "MF", "EnvF", "MLV", "EnvLV", "C"]
+activities = ["mUn", "mEnvU", "mEnvF", "mF", "mLV", "mEnvL", "mFake"]
+
+print(current_state)
+
+for activity in activities:
+    compute_outgoing_states(current_state=current_state, activity=activity, model=model)
+# print(json.dumps(model, indent=4))
